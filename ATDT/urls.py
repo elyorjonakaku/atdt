0ca7e7d09fac4533564e6fba84822f2bd5e4564a
Elyor <41006658+elyorjonakaku@users.noreply.github.com>
2024-10-15 22:34:29 +0500

    Update urls.py

--- ATDT/urls.py
+++ ATDT/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     path('user/',include('user.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
